f401dfdfe638d580aa8bff98d9a6690f864c307c
tresor <tresor@gmail.com>
2023-12-26 19:40:27 +0100

    🧐 Add nodemon and update environment configuration

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -3,12 +3,10 @@ import dotenv from "dotenv";
 dotenv.config();
 
 interface AppConfig {
-  devMod: boolean;
   port: number;
   jwtSecret: string;
   tokenExpiration: string;
   mongoUrl: string;
-  frontUrl: string;
 }
 
 const {
@@ -16,21 +14,15 @@ const {
   PORT = "8080",
   JWT_SECRET,
   TOKEN_EXPIRATION,
-  DEV_MONGO_URL,
-  DEV_FRONT_URL,
-  PROD_MONGO_URL,
-  PROD_FRONT_URL,
+  MONGO_URL,
 } = process.env;
 
-const devMod = DEV_MODE ? JSON.parse(DEV_MODE.toLowerCase()) : false;
 
 const config: AppConfig = {
-  devMod,
   port: parseInt(PORT),
   jwtSecret: JWT_SECRET || "",
   tokenExpiration: TOKEN_EXPIRATION || "",
-  mongoUrl: devMod ? DEV_MONGO_URL || "" : PROD_MONGO_URL || "",
-  frontUrl: devMod ? DEV_FRONT_URL || "" : PROD_FRONT_URL || "",
+  mongoUrl: MONGO_URL || "",
 };
 
 export default config;
--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,11 @@ import { authRoutes } from './routes/auth.routes';
 // import {ticketRoutes} from './routes/ticket.routes';
 // import {eventRoutes} from './routes/event.routes';
 import config from './config/env.config';
+import cors from 'cors';
 
-const {port, frontUrl, mongoUrl} = config;
+const {port, mongoUrl} = config;
 
-if (!port || !frontUrl || !mongoUrl) {
+if (!port || !mongoUrl) {
   console.error('Veuillez renseigner les variables d\'environnement');
   process.exit(1);
 }
@@ -16,13 +17,9 @@ const app: Application = express();
 
 // Middleware
 app.use(express.json());
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', frontUrl);
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
-app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public')); // Pour servir les fichiers statiques
+app.use(express.urlencoded({ extended: true }));0
+app.use(cors());
 
 // Connect to DB
 import './db/mongoDB';
@@ -35,7 +32,7 @@ app.use('/users', userRoutes);
 
 
 app.get('/', (req, res) => {
-  res.send('Bienvenue sur mon API !');
+  res.send('Bienvenue sur gaël api !');
 });
 
 // Start the server
