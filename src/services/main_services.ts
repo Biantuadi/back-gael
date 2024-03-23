7818ab3c3f10cf6131b4951ff2d7c43314775a2a
kevin <biantuadikevin@gmail.com>
2024-03-23 18:15:38 +0100

    ✅ Mise à jour des liens de téléchargement des chansons

--- src/services/main_services.ts
+++ src/services/main_services.ts
@@ -1,22 +1,28 @@
 // import Album from '../models/medias/album.model';
 // import Song from '../models/medias/song.model';
 
-// app.put('/', (req, res) => {
-//     // Mettre à jour songLink
-//     Song.find({}).then((songs) => {
-//       songs.forEach((song:any) => {
-//         const songLink = song.songLink.split('usp=sharing')[0] + 'view?usp=sharing';
-//         // Mettre à jour le songLink pour chaque chanson
-//         song.songLink = songLink;
-//         // Enregistrer les modifications dans la base de données
-//         song.save();
-//       });
-//       res.send('done');
-//     }).catch((err) => {
-//       console.error(err);
-//       res.status(500).send('Erreur lors de la mise à jour des chansons.');
-//     });
-//   });
+/**
+ * app.put('/', (req, res) => {
+  // Rechercher toutes les chansons dans la base de données
+  Song.find({}).then((songs) => {
+    songs.forEach((song:any) => {
+      // Extraire l'ID du fichier à partir du lien original
+      const fileId = song.songLink.split('/d/')[1].split('/view')[0];
+      // Construire le nouveau lien de téléchargement direct
+      const newSongLink = `https://drive.google.com/uc?export=download&id=${fileId}`;
+      // Mettre à jour le songLink pour chaque chanson avec le nouveau lien
+      song.songLink = newSongLink;
+      // Enregistrer les modifications dans la base de données
+
+      song.save();
+    });
+    res.send('done');
+  }).catch((err) => {
+    console.error(err);
+    res.status(500).send('Erreur lors de la mise à jour des chansons.');
+  });
+});
+ */
 
 // ajouter les songs dans les albums respectifs
 /**
