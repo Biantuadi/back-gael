00ff48509673f4b9394f7426d62a930e750d0a0f
kevin <biantuadikevin@gmail.com>
2024-05-17 14:36:07 +0200

    Mettre à jour les routes pour utiliser les contrôleurs génériques

--- src/routes/medias/main.routes.ts
+++ src/routes/medias/main.routes.ts
@@ -6,8 +6,8 @@ const genericRoutes = (model: any) => {
   const router = Router();
   const controller = new GenericController(model);
 
-  router.get("/", AuthMiddleware.isTokenValid, controller.getAll);
-  router.get("/:id", AuthMiddleware.isTokenValid, controller.getById);
+  router.get("/", controller.getAll);
+  router.get("/:id", controller.getById);
   router.post("/", AuthMiddleware.isAdmin, controller.create);
   router.put("/:id", AuthMiddleware.isAdmin, controller.update);
   router.delete("/:id", AuthMiddleware.isAdmin, controller.delete);
